e4bd5ee0a988425e4d903e0424d61f3339a79c6d
Gigi <gomurashvili17@gmail.com>
2025-04-04 16:54:36 +0400

    added some

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-let TitleMain = document.getElementById("Title");
-let Pdescr = document.getElementById("pdescr3");
-let TitleMain2 = document.getElementById("Title3");
-let Pdescr2 = document.getElementById("pdescr4");
-fetch("http://localhost:3000/posts", {
-  method: "GET",
-})
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((element) => {
-      console.log(element);
-      let Title = document.createElement("h3");
-      let descr = document.createElement("p");
-      let Img = document.createElement("img");
-      Img.src = `${element.url}`;
-      Img.classList.add("imagesize");
-      descr.textContent = `${element.text}`;
-      Title.textContent = `${element.title}`;
-      document.getElementById("IconBlock").appendChild(Img);
-      Pdescr.appendChild(descr);
-      TitleMain.appendChild(Title);
-    });
-  })
-  .catch((error) => console.error("Error fetching posts:", error));
+// let TitleMain = document.getElementById("Title");
+// let Pdescr = document.getElementById("pdescr3");
+// let TitleMain2 = document.getElementById("Title3");
+// let Pdescr2 = document.getElementById("pdescr4");
+// fetch("http://localhost:3000/posts", {
+//   method: "GET",
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     data.forEach((element) => {
+//       console.log(element);
+//       let Title = document.createElement("h3");
+//       let descr = document.createElement("p");
+//       let Img = document.createElement("img");
+//       Img.src = `${element.url}`;
+//       Img.classList.add("imagesize");
+//       descr.textContent = `${element.text}`;
+//       Title.textContent = `${element.title}`;
+//       document.getElementById("IconBlock").appendChild(Img);
+//       Pdescr.appendChild(descr);
+//       TitleMain.appendChild(Title);
+//     });
+//   })
+//   .catch((error) => console.error("Error fetching posts:", error));
 
-fetch("http://localhost:3000/comments", {
-  method: "GET",
-})
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((element) => {
-      console.log(element);
-      let Title = document.createElement("h3");
-      let descr = document.createElement("p");
-      let Img = document.createElement("img");
-      Img.src = `${element.url}`;
-      Img.classList.add("imagesize");
-      descr.textContent = `${element.text}`;
-      Title.textContent = `${element.title}`;
-      document.getElementById("IconBlock2").appendChild(Img);
-      Pdescr2.appendChild(descr);
-      TitleMain2.appendChild(Title);
-    });
-  })
-  .catch((error) => console.error("Error fetching posts:", error));
+// fetch("http://localhost:3000/comments", {
+//   method: "GET",
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     data.forEach((element) => {
+//       console.log(element);
+//       let Title = document.createElement("h3");
+//       let descr = document.createElement("p");
+//       let Img = document.createElement("img");
+//       Img.src = `${element.url}`;
+//       Img.classList.add("imagesize");
+//       descr.textContent = `${element.text}`;
+//       Title.textContent = `${element.title}`;
+//       document.getElementById("IconBlock2").appendChild(Img);
+//       Pdescr2.appendChild(descr);
+//       TitleMain2.appendChild(Title);
+//     });
+//   })
+//   .catch((error) => console.error("Error fetching posts:", error));
 
 document.addEventListener("DOMContentLoaded", function () {
   let BurgerBar = document.getElementById("burgerBar");
